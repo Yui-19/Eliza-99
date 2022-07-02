f6be56587d76b0f47c91f2d9e5d0f6858f7fe0d8
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-07-02 09:54:21 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,49 +1,41 @@
 //jshint esversion:8
 const execute = async (client,msg,args) => {
-    // msg.delete(true);
     msg.delete(true);
-    const chat=await msg.getChat();
     let commands =  client.commands;
     if(!args.length){
-        let adminHelp = '🔱 *Administration*\n\n';
-        let infoHelp = '🔱 *Info*\n\n';
-        let pluginHelp = '🔱 *Plugins*\n\n';
-        let groupHelp = '🔱 *Group*\n\n';
-        let ReactionsHelp = '🔱 *Reactions*\n\n';
+        let adminHelp = '*Administration*\n\n';
+        let infoHelp = '*Info*\n\n';
+        let pluginHelp = '*Plugins*\n\n';
         commands.forEach((command) => {
             if(!command.isDependent){
                 if(command.commandType === 'admin')
-                    adminHelp += `⭐ *${command.name} (${command.command})*  - ${command.description}\n`;
+                    adminHelp += `✨ *${command.name} (${command.command})*  - ${command.description}\n\n`;
                 if(command.commandType === 'info')
-                    infoHelp += `⭐ *${command.name} (${command.command})*  - ${command.description}\n`;
+                    infoHelp += `💫 *${command.name} (${command.command})*  - ${command.description}\n\n`;
                 if(command.commandType === 'plugin')
-                    pluginHelp += `⭐ *${command.name} (${command.command})*  - ${command.description}\n`;
-                if(command.commandType === 'group')
-                    groupHelp += `⭐ *${command.name} (${command.command})*  - ${command.description}\n`;
-                if(command.commandType === 'reactions')
-                    ReactionsHelp += `⭐ *${command.name} (${command.command})*  - ${command.description}\n`;
+                    pluginHelp += `🪄 *${command.name} (${command.command})*  - ${command.description}\n\n`;
             }
                 
         });
-        let help = `${infoHelp}\n${pluginHelp}\n${groupHelp}\n${ReactionsHelp}\n${commands.get('help').help}`;
-        await client.sendMessage(chat.id._serialized, help);
+        let help = `${adminHelp}\n${infoHelp}\n${pluginHelp}\n${commands.get('help').help}`;
+        await client.sendMessage(msg.to, help);
     }
 
     else if(commands.has(args[0])){
-        await client.sendMessage(chat.id._serialized, commands.get(args[0]).help);
+        await client.sendMessage(msg.to, commands.get(args[0]).help);
     }
 
     else {
-        await client.sendMessage(chat.id._serialized, `No command with the name *${args[0]}*...`);
+        await client.sendMessage(msg.to, `No command with the name *${args[0]}*...`);
     }
     
 };
 
 module.exports = {
-    name: 'help',
-    description: 'get information about available commands',
+    name: 'Help',
+    description: 'Get information about available commands',
     command: '!help',
     commandType: 'info',
     isDependent: false,
-    help: 'To get more info use ```!help [command]```. Ex: ```!help ping```',
+    help: 'To get more info use ```!help [command]``` Ex : ```!help ping```',
     execute};
