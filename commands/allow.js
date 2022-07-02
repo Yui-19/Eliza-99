a1d47da6bb7a2034e49284c8e16060e7cd319b70
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-07-02 09:47:06 +0530

    Update allow.js

--- commands/allow.js
+++ commands/allow.js
@@ -3,19 +3,18 @@ const config = require("../config");
 const pmpermit = require("../helpers/pmpermit");
 
 const execute = async (client, msg) => {
-  msg.delete(true);
   if (config.pmpermit_enabled == "true" && !msg.to.includes("-")) {
     await pmpermit.permit(msg.to.split("@")[0]);
-    msg.reply("*✅ Allowed*\n\nYou are allowed for PM\n\n_Powered by SciBot_");
+    msg.reply("You have permission to send me personal messages");
   }
 };
 
 module.exports = {
-  name: "Allow for PM",
-  description: "Allow personal messaging for a conatct",
+  name: "Allow",
+  description: "Allows an user to personal message you",
   command: "!allow",
-  commandType: "kjbkj",
+  commandType: "plugin",
   isDependent: false,
-  help: `_You can allow him for pm by these commands_ 👇\n*!allow* - Allow an user for PM\n*!nopm* - Disallow an allowed user for PM`, // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
+  help: `You can allow him for pm by these commands\n\n*!allow* - allows an user to personal message you\n\n*!nopm* - disallows an allowed user to personal message you`,
   execute,
 };
