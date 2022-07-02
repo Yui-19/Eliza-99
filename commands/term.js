b24e566d0f7613a198da44f0ccddd84ccd19596a
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-07-02 10:08:36 +0530

    Create term.js

---
+++ commands/term.js
@@ -0,0 +1,25 @@
+//jshint esversion:8
+
+const { exec } = require('child_process');
+
+const execute = async (client,msg,args) => {
+    msg.delete(true);
+    exec("cd public && " + args.join(' '), async (error, stdout, stderr) => {
+        if (error) {
+            await client.sendMessage(msg.to, "*Eliza~:* ```" + error + "```");
+        } else if (stderr) {
+            await client.sendMessage(msg.to, "*Eliza~:* ```" + stderr + "```");
+        } else {
+            await client.sendMessage(msg.to, "*Eliza~:* ```" + stdout + "```");
+        }
+    });
+};
+
+module.exports = {
+    name: 'Terminal',
+    description: 'Uses teminal remotely',
+    command: '!term',
+    commandType: 'plugin',
+    isDependent: false,
+    help: '*Terminal*\n\nYou can execute any command with this by default it will run from public directory ! If you are leeching something it will be available publicly at\n\n_http://[Your-App-Url]/public_\n\n*!term [command]*\nTo execute a command\n\nEx : ```!term cd/temp/```',
+    execute};
