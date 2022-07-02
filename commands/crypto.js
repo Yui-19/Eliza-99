f2b04c849eaf766be656305ae2af7566d417e3d0
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-07-02 09:50:13 +0530

    Create crypto.js

---
+++ commands/crypto.js
@@ -0,0 +1,52 @@
+//jshint esversion:8
+//Coded by Sumanjay (https://github.com/cyberboysumanjay)
+const axios = require('axios');
+
+async function getPrice(cryptoCode) {
+    cryptoCode = cryptoCode.toUpperCase();
+    let mainconfig = {
+        method: 'get',
+        url: 'https://public.coindcx.com/market_data/current_prices'
+    };
+    return axios(mainconfig)
+        .then(async function (response) {
+            let data = response.data;
+            let cryptoCodeINR = cryptoCode + "INR";
+            if (data[cryptoCode] != undefined || data[cryptoCodeINR] != undefined) {
+                cryptoCode = data[cryptoCode] == undefined ? cryptoCodeINR : cryptoCode;
+                let out = ({
+                    name: cryptoCode,
+                    price: data[cryptoCode]
+                });
+                return out;
+            } else {
+                return "unsupported";
+            }
+        })
+        .catch(function (error) {
+            return "error";
+        });
+}
+const execute = async (client,msg,args) => {
+    msg.delete(true);
+    let data = await getPrice(args[0]);
+    if (data == "error") {
+        await client.sendMessage(msg.to, `*Error*\n\n` + "```Something unexpected happened while fetching cryptocurrency price```");
+    }
+    if (data == "unsupported") {
+        await client.sendMessage(msg.to, `*Error*\n\n` + "```Support for this crypto currency is not yet added```");
+    }
+    else {
+        let date = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+        await client.sendMessage(msg.to, `Price of *${data.name}* as of ${date} is *₹ ${data.price}*`);
+    }
+};
+
+module.exports = {
+    name: 'Crypto currency',
+    description: 'Gets price info for requested crypto currency',
+    command: '!crypto',
+    commandType: 'plugin',
+    isDependent: false,
+    help: `*Crypto currency*\n\nGet current price of crypto currency\n\n*!crypto [crypto-code]*\n`,
+    execute};
